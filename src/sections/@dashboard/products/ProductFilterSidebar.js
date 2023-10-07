619799ca9613b73a75c1be8fe30eb32a0e726e96
ivanbecerrq <becerraandres124@gmail.com>
2023-10-07 10:51:59 -0500

    traduci el idioma del filters

--- src/sections/@dashboard/products/ProductFilterSidebar.js
+++ src/sections/@dashboard/products/ProductFilterSidebar.js
@@ -28,13 +28,13 @@ export const SORT_BY_OPTIONS = [
   { value: 'priceDesc', label: 'Price: High-Low' },
   { value: 'priceAsc', label: 'Price: Low-High' },
 ];
-export const FILTER_GENDER_OPTIONS = ['Men', 'Women', 'Kids'];
-export const FILTER_CATEGORY_OPTIONS = ['All', 'Shose', 'Apparel', 'Accessories'];
+export const FILTER_GENDER_OPTIONS = ['hombre', 'mujer', 'niños'];
+export const FILTER_CATEGORY_OPTIONS = ['todo', 'zapatos', 'vestir', 'accesorios'];
 export const FILTER_RATING_OPTIONS = ['up4Star', 'up3Star', 'up2Star', 'up1Star'];
 export const FILTER_PRICE_OPTIONS = [
-  { value: 'below', label: 'Below $25' },
-  { value: 'between', label: 'Between $25 - $75' },
-  { value: 'above', label: 'Above $75' },
+  { value: 'abajo', label: 'abajo $25' },
+  { value: 'entre', label: 'entre $25 - $75' },
+  { value: 'ariba', label: 'ariba $75' },
 ];
 export const FILTER_COLOR_OPTIONS = [
   '#00AB55',
